e6ff994eaf47bb3b7b7e5e74b6e0e05ab29766d7
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-21 18:03:56 +0200

    Registrando componente Items de la Factura

---
+++ src/main/java/com/angel/curso/springboot/factura/springbootdifactura/AppConfig.java
@@ -0,0 +1,23 @@
+package com.angel.curso.springboot.factura.springbootdifactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.angel.curso.springboot.factura.springbootdifactura.models.Item;
+import com.angel.curso.springboot.factura.springbootdifactura.models.Product;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+    @Bean
+    List<Item> itemsInvoice() {
+        Product p1 = new Product("Camara", 300);
+        Product p2 = new Product("Bicicleta", 1300);
+        return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
+    }
+
+}
